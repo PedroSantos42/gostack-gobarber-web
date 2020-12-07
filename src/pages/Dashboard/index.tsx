21be9872c46ea45f386d0d711bc291c954849d97
pedro.santos <pedroabs.dev42@gmail.com>
2020-12-07 16:38:23 -0300

    add Month Availability to Calendar

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useMemo, useState } from 'react';
 import DayPicker, { DayModifiers } from 'react-day-picker';
 import 'react-day-picker/lib/style.css';
 
@@ -18,17 +18,57 @@ import {
 } from './styles';
 
 import logoImg from '../../assets/logo.svg';
-
 import { useAuth } from '../../hooks/auth';
+import api from '../../services/api';
+
+interface MonthAvailabiltyItem {
+  day: number;
+  available: boolean;
+}
 
 const Dashboard: React.FC = () => {
+  const { signOut, user } = useAuth();
+
   const [selectedDate, setSelectedDate] = useState(new Date());
+  const [currentMonth, setCurrentMonth] = useState(new Date());
+
+  const [monthAvailability, setMonthAvailabilty] = useState<
+    MonthAvailabiltyItem[]
+  >([]);
 
   const handleDateChange = useCallback((day: Date, modifiers: DayModifiers) => {
     if (modifiers.available) setSelectedDate(day);
   }, []);
 
-  const { signOut, user } = useAuth();
+  const handleMonthChange = useCallback((month: Date) => {
+    setCurrentMonth(month);
+  }, []);
+
+  useEffect(() => {
+    api
+      .get(`/providers/${user.id}/month-availability`, {
+        params: {
+          year: currentMonth.getFullYear(),
+          month: currentMonth.getMonth() + 1,
+        },
+      })
+      .then(response => {
+        setMonthAvailabilty(response.data);
+      });
+  }, [currentMonth, user]);
+
+  const disabledDays = useMemo(() => {
+    const dates = monthAvailability
+      .filter(monthDay => monthDay.available === false)
+      .map(monthDay => {
+        const year = currentMonth.getFullYear();
+        const month = currentMonth.getMonth();
+
+        return new Date(year, month, monthDay.day);
+      });
+
+    return dates;
+  }, [currentMonth, monthAvailability]);
 
   return (
     <Container>
@@ -48,18 +88,42 @@ const Dashboard: React.FC = () => {
             <FiPower />
           </button>
         </HeaderContent>
+      </Header>
 
-        <Content>
-          <Schedule>
-            <h1>Horários agendados</h1>
-            <p>
-              <span>Hoje</span>
-              <span>Dia 06</span>
-              <span>Segunda-feira</span>
-            </p>
+      <Content>
+        <Schedule>
+          <h1>Horários agendados</h1>
+          <p>
+            <span>Hoje</span>
+            <span>Dia 06</span>
+            <span>Segunda-feira</span>
+          </p>
 
-            <NextAppointment>
-              <strong>Atendimento a seguir</strong>
+          <NextAppointment>
+            <strong>Atendimento a seguir</strong>
+            <div>
+              <img
+                src="https://avatars3.githubusercontent.com/u/38994990?s=460&u=b174c927aaba3366449ce0fcfa1a0e0d07e25fae&v=4"
+                alt="Pedro Santos"
+              />
+
+              <strong>Pedro Santos</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
               <div>
                 <img
                   src="https://avatars3.githubusercontent.com/u/38994990?s=460&u=b174c927aaba3366449ce0fcfa1a0e0d07e25fae&v=4"
@@ -67,95 +131,72 @@ const Dashboard: React.FC = () => {
                 />
 
                 <strong>Pedro Santos</strong>
-                <span>
-                  <FiClock />
-                  08:00
-                </span>
               </div>
-            </NextAppointment>
+            </Appointment>
 
-            <Section>
-              <strong>Manhã</strong>
+            <strong>Tarde</strong>
 
-              <Appointment>
-                <span>
-                  <FiClock />
-                  08:00
-                </span>
+            <Appointment>
+              <span>
+                <FiClock />
+                13:00
+              </span>
 
-                <div>
-                  <img
-                    src="https://avatars3.githubusercontent.com/u/38994990?s=460&u=b174c927aaba3366449ce0fcfa1a0e0d07e25fae&v=4"
-                    alt="Pedro Santos"
-                  />
+              <div>
+                <img
+                  src="https://avatars3.githubusercontent.com/u/38994990?s=460&u=b174c927aaba3366449ce0fcfa1a0e0d07e25fae&v=4"
+                  alt="Pedro Santos"
+                />
 
-                  <strong>Pedro Santos</strong>
-                </div>
-              </Appointment>
+                <strong>Pedro Santos</strong>
+              </div>
+            </Appointment>
 
-              <strong>Tarde</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                14:00
+              </span>
 
-              <Appointment>
-                <span>
-                  <FiClock />
-                  13:00
-                </span>
+              <div>
+                <img
+                  src="https://avatars3.githubusercontent.com/u/38994990?s=460&u=b174c927aaba3366449ce0fcfa1a0e0d07e25fae&v=4"
+                  alt="Pedro Santos"
+                />
 
-                <div>
-                  <img
-                    src="https://avatars3.githubusercontent.com/u/38994990?s=460&u=b174c927aaba3366449ce0fcfa1a0e0d07e25fae&v=4"
-                    alt="Pedro Santos"
-                  />
-
-                  <strong>Pedro Santos</strong>
-                </div>
-              </Appointment>
-
-              <Appointment>
-                <span>
-                  <FiClock />
-                  14:00
-                </span>
-
-                <div>
-                  <img
-                    src="https://avatars3.githubusercontent.com/u/38994990?s=460&u=b174c927aaba3366449ce0fcfa1a0e0d07e25fae&v=4"
-                    alt="Pedro Santos"
-                  />
-
-                  <strong>Pedro Santos</strong>
-                </div>
-              </Appointment>
-            </Section>
-          </Schedule>
-          <Calendar>
-            <DayPicker
-              weekdaysShort={['D', 'S', 'T', 'Q', 'Q', 'S', 'S']}
-              fromMonth={new Date()}
-              disabledDays={[{ daysOfWeek: [0, 6] }]}
-              modifiers={{
-                available: { daysOfWeek: [1, 2, 3, 4, 5] },
-              }}
-              onDayClick={handleDateChange}
-              selectedDays={selectedDate}
-              months={[
-                'Janeiro',
-                'Fevereiro',
-                'Março',
-                'Abril',
-                'Maio',
-                'Junho',
-                'Julho',
-                'Agosto',
-                'Setembro',
-                'Outubro',
-                'Novembro',
-                'Dezembro',
-              ]}
-            />
-          </Calendar>
-        </Content>
-      </Header>
+                <strong>Pedro Santos</strong>
+              </div>
+            </Appointment>
+          </Section>
+        </Schedule>
+        <Calendar>
+          <DayPicker
+            weekdaysShort={['D', 'S', 'T', 'Q', 'Q', 'S', 'S']}
+            fromMonth={new Date()}
+            disabledDays={[{ daysOfWeek: [0, 6] }, ...disabledDays]}
+            modifiers={{
+              available: { daysOfWeek: [1, 2, 3, 4, 5] },
+            }}
+            onMonthChange={handleMonthChange}
+            onDayClick={handleDateChange}
+            selectedDays={selectedDate}
+            months={[
+              'Janeiro',
+              'Fevereiro',
+              'Março',
+              'Abril',
+              'Maio',
+              'Junho',
+              'Julho',
+              'Agosto',
+              'Setembro',
+              'Outubro',
+              'Novembro',
+              'Dezembro',
+            ]}
+          />
+        </Calendar>
+      </Content>
     </Container>
   );
 };
