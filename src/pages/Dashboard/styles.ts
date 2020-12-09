a54cb003c60cf2ab33f85e49c0e24a2ebfd377e8
pedro.santos <pedroabs.dev42@gmail.com>
2020-12-09 10:19:41 -0300

    create Profile page

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -52,8 +52,15 @@ export const Profile = styled.div`
       color: #f4ede8;
     }
 
-    > strong {
+    a {
       color: #ff9000;
+      text-decoration: none;
+
+      transition: opacity 0.2s;
+
+      &:hover {
+        opacity: 0.8;
+      }
     }
   }
 `;
